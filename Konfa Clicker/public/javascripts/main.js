fb6bff5ea995ba7e4e77980d8710a5056e235b03
Yep97 <Yep97@LAPTOP-2GH1SFLU>
2020-08-30 19:59:25 +0200

    Second update

--- Konfa Clicker/public/javascripts/main.js
+++ Konfa Clicker/public/javascripts/main.js
@@ -1,3 +1,5 @@
+import { powerUp } from './powers.js'
+
 let pressStart = document.getElementById("pressStart");
 let startSection = document.getElementById("startSection");
 let body = document.getElementById("body");
@@ -6,24 +8,13 @@ let header = document.getElementById("header");
 let mainButton = document.getElementById("mainButton");
 let leftSide = document.getElementById("leftSide");
 let points = document.getElementById("points");
-let firstPower = document.getElementById("firstPower");
-let firstPowerCost = document.getElementById("firstPowerCost");
-let firstPowerUpgradeCost = document.getElementById("firstPowerUpgradeCost");
-let firstPowerCounter = document.getElementById("firstPowerCounter");
 
-let memeValue = 10;
+//constructor(power, powerCost, powerUpgradeCost, powerCounter, powerPrice, powerValue, powerUpgradeValue, powerUpgradePrice, powerUpgrade)
+let firstPower = new powerUp("firstPower", "firstPowerCost", "firstPowerUpgradeCost", "firstPowerCounter", 10, 0, 1, 500, "firstPowerUprade");
+
+let memeValue = 0;
 let clickValue = 1;
-let firstPowerPrice = 10;
 let timeCounter = 1;
-let firstPowerValue = 0;
-let firstPowerUpgrade = 1;
-let firstPowerUpgradePrice = 500;
-
-
-//firstPowerValue.value = firstPowerPrice;
-firstPowerCost.innerHTML = firstPowerPrice;
-firstPowerUpgradeCost.innerHTML = firstPowerUpgradePrice;
-firstPowerCounter.innerHTML = 0;
 
 showGame();
 
@@ -38,7 +29,7 @@ pressStart.onclick = function () {
 
 function showGame() {
     if (gameWidnow.style.display === "none") {
-//        header.style.opacity = '1';
+        //        header.style.opacity = '1';
         gameWidnow.style.display = "block";
     } else {
         gameWidnow.style.display = "none";
@@ -48,48 +39,22 @@ function showGame() {
 mainButton.onclick = function () {
     memeValue += clickValue;
     points.innerHTML = memeValue;
-    opacityOfButtons();
 }
 
-firstPower.onclick = function () {
-    if (memeValue >= firstPowerPrice) {
-        firstPower.style.opacity = '1';
-        memeValue -= firstPowerPrice;
-        firstPowerValue += 1;
-        firstPowerPrice += Math.round(Math.pow(firstPowerPrice, 0.7));
-        points.innerHTML = memeValue;
-        firstPowerCost.innerHTML = firstPowerPrice;
-        firstPowerCounter.innerHTML++;
-        
-    } else {
-        firstPower.style.opacity = '0.5';
-    }
-}
 
-pawel.onclick = function () {
-    if (memeValue >= firstPowerUpgradePrice) {
-        memeValue -= firstPowerUpgradePrice;
-        firstPowerUpgrade = 2;
-        pawel.disabled = true;
-    }
-}
-
-function opacityOfButtons() {
-    if (memeValue >= firstPowerPrice) {
-        firstPower.style.opacity = '1';
-    } else {
-        firstPower.style.opacity = '0.5';      
-    }
-}
 
 function timeLoop() {
     setTimeout(function () {
         //Meme Value Gain 
         points.innerHTML = memeValue;
-        memeValue += firstPowerValue * firstPowerUpgrade;
-        
-        opacityOfButtons();
+        memeValue += firstPower.powerValue * firstPower.powerUpgradeValue;
 
+        document.getElementById("firstPower").onclick = function () { firstPower.powerClick(points, memeValue) };
+
+        document.getElementById("firstPowerUpgrade").onclick = function () { firstPower.powerUpgradeClick(memeValue) };
+
+
+        firstPower.opacityOfButtons(memeValue);
 
         timeCounter++;
         if (timeCounter < Infinity) {
@@ -100,6 +65,5 @@ function timeLoop() {
 
 
 window.onload = function start() {
-
     timeLoop();
 }
